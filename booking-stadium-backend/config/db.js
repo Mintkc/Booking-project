9b29fd3957e1e010809428049aafc0b92baadb5e
Mintkc <krittiluck07@gmail.com>
2025-09-13 21:10:23 +0700

    อัพเดพ seeds/และแก้ไข config/db.js

--- booking-stadium-backend/config/db.js
+++ booking-stadium-backend/config/db.js
@@ -1,15 +1,15 @@
 import mongoose from 'mongoose';
 
 const connectDB = async () => {
-    try {
-        await mongoose.connect(process.env.MONGO_URI, {
-            dbName: 'database',   // บังคับใช้ DB ชื่อ "database"
-        });
-        console.log('MongoDB connected');
-    } catch (error) {
-        console.error('MongoDB connection error:', error);
-        process.exit(1);
-    }
+  try {
+    await mongoose.connect(process.env.MONGO_URI, {
+      dbName: 'booking'
+    });
+    console.log('✅ MongoDB connected');
+  } catch (error) {
+    console.error('❌ MongoDB connection error:', error.message);
+    process.exit(1);
+  }
 };
 
 export default connectDB;
